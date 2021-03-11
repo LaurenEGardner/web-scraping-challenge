db4d9bc569e2afba6cc483925a0569b304bcaa77
LaurenEGardner <heylo.gardner@gmail.com>
2021-03-11 15:35:27 -0800

    everything but featured image currently working

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars as sm
+import requests
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
-collection = mongo.db.destination
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-# Route to render index.html template using data from Mongo
+# Route to render index.html template
 @app.route("/")
 def home():
 
-    # Find one record of data from the mongo database
-    # @TODO: YOUR CODE HERE!
-    mars_data = collection.find_one()
-    print(mars_data)
+    marsInfo = mongo.db.marsInfo.find_one()
+    return render_template("index.html", marsInfo=marsInfo)
 
-    # Return template and data
-    return render_template("index.html", mars=mars_data)
+# Route that will trigger the web scrape function
+@app.route("/scrape")
+def scrape():
+
+    marsInfo = mongo.db.marsInfo
+
+    # Run scrape functions and save the data
+    marsData = sm.scrapeMars()
+    marsInfo.update({}, marsData, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -3,15 +3,19 @@ from bs4 import BeautifulSoup as bs
 import time
 from pprint import pprint
 import requests
+import pandas as pd
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
     executable_path = {"executable_path": "C:/Users/Wayne Gardner/Desktop/chromedriver/chromedriver.exe"}
     return Browser("chrome", **executable_path, headless=False)
 
-def scrape_info():
-    browser = init_browser()
 
+def scrapeMars():
+    browser = init_browser()
+    #################
+    # Scraping news #
+    #################
     # Visit the nasa mars news website
     url = "https://mars.nasa.gov/news"
     browser.visit(url)
@@ -31,24 +35,30 @@ def scrape_info():
     article = soup.find_all("div", class_="article_teaser_body")
     news_p=article[0].text
     
-    # Visit the url for JPL Featured Space Image
-    url2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    browser.visit(url2)
+    ###########################
+    # Scraping Featured Image #
+    ###########################
+    #NASA changed website so this doesn't work atm 
+    # # Visit the url for JPL Featured Space Image
+    # url2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    # browser.visit(url2)
 
-    # Pause for page to load
-    time.sleep(1)
+    # # Pause for page to load
+    # time.sleep(1)
 
-    # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
+    # # Scrape page into Soup
+    # html = browser.html
+    # soup = bs(html, "html.parser")
     
-    #navigate the site and find the image url for the current Featured Mars Image
-    #and assign the url string to a variable called featured_image_url.
-    relative_image_path = soup.find('article', class_="carousel_item")['style']
-    relative_image_path_url=relative_image_path.split("('", 1)[1].split("')")[0]
-    featured_image_url = "https://www.jpl.nasa.gov/" + relative_image_path_url
+    # #navigate the site and find the image url for the current Featured Mars Image
+    # #and assign the url string to a variable called featured_image_url.
+    # relative_image_path = soup.find('article', class_="carousel_item")['style']
+    # relative_image_path_url=relative_image_path.split("('", 1)[1].split("')")[0]
+    # featured_image_url = "https://www.jpl.nasa.gov/" + relative_image_path_url
     
-    
+    ##################
+    # Scraping facts #
+    ##################
     # Visit the Mars Facts webpage
     url3 = "https://space-facts.com/mars/"
     browser.visit(url3)
@@ -74,18 +84,21 @@ def scrape_info():
     
     
     #building fact table
-    mars_facts={
-        "Equatorial Diameter":eq_diam,
-        "Polar Diameter":polar_diam,
-        "Mass":mass,
-        "Moons":moons,
-        "Orbit Distance":orb_dist,
-        "Orbit Period":orb_period,
-        "Surface Temp":surf_temp,
-        "First Record":first_rec,
-        "Recorded By":rec_by
-    }
-    
+    mars_facts=[
+        {"key" : "Equatorial Diameter", "value" :eq_diam},
+        {"key" : "Polar Diameter", "value" :polar_diam},
+        {"key" : "Mass", "value" :mass},
+        {"key" : "Moons", "value" :moons},
+        {"key" : "Orbit Distance", "value" :orb_dist},
+        {"key" : "Orbit Period", "value" :orb_period},
+        {"key" : "Surface Temp", "value" :surf_temp},
+        {"key" : "First Record", "value" :first_rec},
+        {"key" : "Recorded By", "value" :rec_by}
+    ]
+
+    #######################
+    # Scraping Hem images #
+    #######################
     # Visit the USGS Astrogeology site 
     url4 = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url4)
@@ -130,13 +143,16 @@ def scrape_info():
     {"title": "Syrtis Major Hemisphere", "img_url": base_url + hem_imgs[2]},
     ]
     
-    #print just to see if it's working
-    print(news_title)
-    print("-----")
-    print(news_p)
-    print("-----")
-    print(featured_image_url)
-    print("-----")
-    print(mars_facts)
-    print("-----")
-    print(hemisphere_image_urls)
+    # Quit the browser after scraping
+    browser.quit()
+
+    mars_data = {
+        "news_p": news_p,
+        "news_title": news_title,
+        "mars_facts" : mars_facts, 
+        # "featured_image": featured_image_url,
+        "hemisphere_image": hemisphere_image_urls
+    }
+
+    # Return results
+    return mars_data
